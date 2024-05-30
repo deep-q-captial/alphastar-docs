8028363ce2d7dbb74f16245fdfe189abc620f780
nhenke-deepq <niklas.henke@deepqdigital.com>
2024-05-30 13:54:39 +0000

    Add header signing

--- examples/signing.py
+++ examples/signing.py
@@ -1,6 +1,8 @@
+import json
 import os
 
 from dotenv import load_dotenv
+from eth_account.messages import encode_defunct
 from web3 import Account
 
 account = None
@@ -17,3 +19,16 @@ def get_account() -> Account:
         account = Account.from_key(os.getenv("WALLET_PRIVATE_KEY"))
 
     return account
+
+
+def sign_auth_headers(data: dict = {}):
+    """
+    Signs the given data with your wallet to authenticate requests to the API.
+    """
+
+    account = get_account()
+
+    data_enc = encode_defunct(json.dumps(data))
+    data_enc = account.sign_message(data_enc)
+
+    return {"wallet": account.address, "signature": data_enc.signature.hex()}
